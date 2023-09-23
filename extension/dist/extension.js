67f94957d35e2053f254d93a3cb3c071d5074a62
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-09-24 00:41:55 +0300

    feat: init extension

---
+++ extension/dist/extension.js
@@ -0,0 +1,21 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.deactivate = exports.activate = void 0;
+function activate(context) { }
+exports.activate = activate;
+// This method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
---
+++ extension/src/extension.ts
@@ -0,0 +1,6 @@
+import * as vscode from "vscode"
+
+export function activate(context: vscode.ExtensionContext) {}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
