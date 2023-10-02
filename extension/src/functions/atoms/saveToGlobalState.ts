7f8906935df9db4691390b8e58f7709e0f89e8c1
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-10-02 16:20:06 +0300

    fix: bug fix

--- extension/src/functions/atoms/saveToGlobalState.ts
+++ extension/src/functions/atoms/saveToGlobalState.ts
@@ -9,13 +9,19 @@ const saveToGlobalState = (
 ) => {
   const isExist = ctx.globalState.get(payload.key)
 
+  /* 
+  
+  using string.valueOf() prevents values saving in globalState object like "'value'"
+  
+  */
+
   if (isExist) {
-    ctx.globalState.update(payload.key, payload.value)
+    ctx.globalState.update(payload.key, payload.value.valueOf())
     return ctx.globalState.get(payload.key)
   }
 
   ctx.globalState.setKeysForSync([payload.key])
-  ctx.globalState.update(payload.key, payload.value)
+  ctx.globalState.update(payload.key, payload.value.valueOf())
   return ctx.globalState.get(payload.key)
 }
 
