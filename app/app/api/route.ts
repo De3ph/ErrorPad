a862a2e45e940c33581a0ead6b7941ee07265079
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-10-07 14:43:22 +0300

    fix: read req body

--- app/app/api/route.ts
+++ app/app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export async function POST(req: NextRequest) {
-  console.log(req.json())
+  console.log(await req.json())
   return NextResponse.json("hi")
 }
