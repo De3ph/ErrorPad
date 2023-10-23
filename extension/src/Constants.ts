8797fa1d70a660d0fb3d37033e3702bc8b64ff5b
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-10-23 11:34:24 +0300

    feat: documentation imprs

--- extension/src/Constants.ts
+++ extension/src/Constants.ts
@@ -1,3 +1,5 @@
+// using Object.freeze protects the object from unwanted changes, nested objects protecting too
+
 const CONSTANTS = Object.freeze({
   usernameKey: "errorpad.username",
   passwordKey: "errorpad.password",
